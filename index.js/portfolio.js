1107396ad74f58bb01c7add8002112725dfa1f9d
iRobot90 <amiandajesse27@gmail.com>
2023-03-02 14:27:53 +0300

    added js

---
+++ index.js/portfolio.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text",{
+    strings:["FullStack Developer","Basketballer","Footballer","Spoken-word artist","Poet"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop:true,
+    
+
+})
